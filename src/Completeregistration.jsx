656d82d37d7db0c0064a1f8f1f99dac445d943bb
Akande Al-ameen <longlasting1805@gmail.com>
2023-07-21 15:29:09 +0100

    completeregistration image refactor

--- src/Completeregistration.jsx
+++ src/Completeregistration.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import imgUrl from './assets/ygt.JPG'
+import imgUrl from './assets/com.jpg'
 import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
 
@@ -11,27 +11,28 @@ export const Completeregistration = (props) => {
     }
     return(
 
-        <div className="root-div">
+    <div className="root-div">
         <div className="img">
             <img src={imgUrl}alt="Italian Trulli"/>
         </div>
 
         <div className="select-identification">
-            <p>KYC</p>
-            <h1>Select a Form of Identification</h1>
-        <FormControl fullWidth>
-            <InputLabel onChange={(e) => setNin(e.target.value)} id="demo-simple-select-label">Nin</InputLabel>
-            <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={nin}
-                label="Nin"
-                onChange={handleChange}
-            >
-                <MenuItem value={10}>Ten</MenuItem>
-            
-            </Select>
-        </FormControl>
+            <p className="kyc">KYC</p>
+            {/* <p className="kyc">kindly complete your registration</p> */}
+            <h2>Select a Form of Identification</h2>
+            <FormControl fullWidth>
+                <InputLabel onChange={(e) => setNin(e.target.value)} id="demo-simple-select-label">Nin</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={nin}
+                    label="Nin"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                
+                </Select>
+            </FormControl>
         </div>
 
     </div>
