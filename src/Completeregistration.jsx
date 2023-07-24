55acfad804e8eafcf4bc19da74f62a64b75f1289
Akande Al-ameen <longlasting1805@gmail.com>
2023-07-24 12:22:43 +0100

    add cards

--- src/Completeregistration.jsx
+++ src/Completeregistration.jsx
@@ -3,9 +3,13 @@ import imgUrl from './assets/com.jpg'
 import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
 
+
 export const Completeregistration = (props) => {
 
     const [nin, setNin] = useState('');
+    const [driverslicence, setDriversLicence] = useState('');
+    const [voterscard, setVotersCard] = useState('');
+    const [homeaddress, setHomeAdress] = useState('');
     const handleChange = (e) =>{
         e.preventDefault()
     }
@@ -16,12 +20,18 @@ export const Completeregistration = (props) => {
             <img src={imgUrl}alt="Italian Trulli"/>
         </div>
 
+        
         <div className="select-identification">
+        <div className="kindly">
+            <p>Kindly Complete your Registration</p>
+        </div>
+
             <p className="kyc">KYC</p>
-            {/* <p className="kyc">kindly complete your registration</p> */}
-            <h2>Select a Form of Identification</h2>
             <FormControl fullWidth>
-                <InputLabel onChange={(e) => setNin(e.target.value)} id="demo-simple-select-label">Nin</InputLabel>
+                <p className="select-class">select a form of identification</p>
+                <InputLabel onChange={(e) => setNin(e.target.value)} id="demo-simple-select-label" placeholder="nin"></InputLabel>
+                <InputLabel onChange={(e) => setDriversLicence(e.target.value)} id="demo-simple-select-label" placeholder="drivers licence"></InputLabel>
+                <InputLabel onChange={(e) => setVotersCard(e.target.value)} id="demo-simple-select-label" placeholder="voters card"></InputLabel>
                 <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
@@ -29,10 +39,20 @@ export const Completeregistration = (props) => {
                     label="Nin"
                     onChange={handleChange}
                 >
-                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={nin}>Nin</MenuItem>
+                    <MenuItem value={driverslicence}>Drivers licence</MenuItem>
+                    <MenuItem value={voterscard}>Voters card</MenuItem>
                 
                 </Select>
+               
             </FormControl>
+            <div>
+            <input className="homeaddress" value={homeaddress}  onChange={(e) => setHomeAdress(e.target.value)} placeholder="Home Address"/>
+            </div>
+
+            <div className="cards">
+            <p>Add Cards</p>
+            </div>
         </div>
 
     </div>
