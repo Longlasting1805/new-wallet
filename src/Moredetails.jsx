d6cd0dfcfcadb9e857a985b09303ba4e812706b3
Akande Al-ameen <longlasting1805@gmail.com>
2023-07-26 15:55:47 +0100

    Moredetails page

--- src/Moredetails.jsx
+++ src/Moredetails.jsx
@@ -19,13 +19,32 @@ export const Moredetails = (props) => {
             <div className="image">
             <img src={imgUrl}alt="Italian Trulli"/>
             </div>
+
+            
+
             <div className="details">
+            <p className="more-details">More Details about you!</p>
+            <p className="nextkin">Next of Kin</p>
                 <input className="fullname" value={fullname}  onChange={(e) => setFullName(e.target.value)} placeholder="full name"/>
                 <input className="email" value={email}  onChange={(e) => setEmail(e.target.value)} placeholder="email"/>
                 <input className="phonenumber" value={phonenumber}  onChange={(e) => setPhonenumber(e.target.value)} placeholder="phone number"/>
                 <input className="relationship" value={relationship}  onChange={(e) => setRelationship(e.target.value)} placeholder="relationship"/>
+            <div>
+             <p className="bvn">BVN</p>
+             <input className="bvn-tag" value={bvn}  onChange={(e) => setBvn(e.target.value)} placeholder="bvn"/>
             </div>
+            <button className="continue">Continue</button>
+            <div className="prev">
+            <a href="/prev" className="next" >Prev</a>
+            </div>
+            
+            </div>
+           
+
+
         </div>
+        
+        
       )
 
 }
