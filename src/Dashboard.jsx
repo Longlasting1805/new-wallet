c81dfda0133aeb28be7b9e3fa7394620c580a9db
Akande Al-ameen <longlasting1805@gmail.com>
2023-08-17 11:36:59 +0100

    dashboard card

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -4,6 +4,10 @@ import imgUrl1 from "./assets/group1.jpg";
 // import { Checkbox } from "@mui/material";
 // import { useNavigate } from "react-router-dom";
 import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
@@ -35,10 +39,34 @@ export const Dashboard = (props) => {
                     <img className="money-img" src={imgUrl}alt="Trulli"/>
                     <img className="money-img" src={imgUrl}alt="Trulli"/>
                     <img className="money-img" src={imgUrl}alt="Trulli"/>
-                </div>
-        <div>
-        <Card variant="outlined">{card}</Card>
+
+                    <div className="card-div">
+          <Card className="root-card">
+              <CardContent className="card-content">
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                  Word of the Day
+                </Typography>
+                <Typography variant="h5" component="div">
+                  be{card}nev{card}o{card}lent
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  adjective
+                </Typography>
+                <Typography variant="body2">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+        </Card>
+      
+  
         </div>
+                </div>
+       
   </div>
   );
 };
