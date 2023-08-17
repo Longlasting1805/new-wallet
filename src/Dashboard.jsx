50a14b53703c6fdaaf5405b514f52173968291ca
Akande Al-ameen <longlasting1805@gmail.com>
2023-08-17 14:15:41 +0100

    images added

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import imgUrl from "./assets/vector1.jpg";
 import imgUrl1 from "./assets/group1.jpg";
+import imgUrl2 from "./assets/atmCard.jpg";
+import imgUrl3 from "./assets/handMoney.jpg";
+import imgUrl4 from "./assets/help.jpg";
+import imgUrl5 from "./assets/airtime.jpg";
+import imgUrl6 from "./assets/save.jpg";
+
 // import { Checkbox } from "@mui/material";
 // import { useNavigate } from "react-router-dom";
 import Card from '@mui/material/Card';
@@ -55,13 +61,24 @@ export const Dashboard = (props) => {
                     <p>£1m</p>
                   </div>
 
-                  <div>
-                    
-                  </div>
-
                 </CardContent>
                </div>
               </div>
+              <div className="images-div">
+                    <img className="atm-img" src={imgUrl2}alt="Trulli"/>
+                    <p>cards</p>
+                    <img className="spend-img" src={imgUrl3}alt="Trulli"/>
+                    <p>Spend</p>
+                    <img className="help-img" src={imgUrl4}alt="Trulli"/>
+                    <p>Help</p>
+                    <img className="airtime-img" src={imgUrl5}alt="Trulli"/>
+                    <p>Airtime</p>
+                    <img className="save-img" src={imgUrl6}alt="Trulli"/>
+                    <p>Save</p>
+
+              </div>
+
+              
               
                
               </CardContent>
