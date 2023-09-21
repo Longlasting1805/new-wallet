f6567f099d8620eecfa9ab2bd4193339657a54c0
Akande Al-ameen <longlasting1805@gmail.com>
2023-09-21 19:18:37 +0100

    card styled

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -14,6 +14,7 @@ import imgUrl12 from "./assets/user.jpg";
 import imgUrl13 from "./assets/bell.jpg";
 import imgUrl14 from "./assets/eye.jpg";
 import imgUrl15 from "./assets/WebdashboardPage.jpg"
+import imgUrl16 from  "./assets/arrow.jpg"
 
 // import { Checkbox } from "@mui/material";
 // import { useNavigate } from "react-router-dom";
@@ -62,24 +63,24 @@ export const Dashboard = (props) => {
                     <p className="card-text">Cards</p>
                   </div>
                   <div>
-                    <img src={imgUrl3} />
-                    <p className="card-text">Cards</p>
+                    <img src={imgUrl4} />
+                    <p className="card-text">Spend</p>
                   </div>
                   <div>
-                    <img src={imgUrl3} />
-                    <p className="card-text">Cards</p>
+                    <img src={imgUrl5} />
+                    <p className="card-text">Help</p>
                   </div>
                   <div>
-                    <img src={imgUrl3} />
-                    <p className="card-text">Cards</p>
+                    <img src={imgUrl6} />
+                    <p className="card-text">Airtime</p>
                   </div>
                   <div>
-                    <img src={imgUrl3} />
-                    <p className="card-text">Cards</p>
+                    <img src={imgUrl7} />
+                    <p className="card-text">Save</p>
                   </div>
                 </div>
                 <div>
-                  <img src={imgUrl} />
+                  <img className="arrow" src={imgUrl16} />
                 </div>
               </div>
               <div>
@@ -100,95 +101,55 @@ export const Dashboard = (props) => {
                   </div>
                 </div>
               </div>
+              <div>
+                  <div className="recent-trans">
+                    <div>
+                      <p className="date">3-28-2022</p>
+                      <img src={imgUrl9} />
+                    </div>
+                    <div className="water-time">
+                      <p className="water">Airtime</p>
+                      <p className="time">10:00 am</p>
+                    </div>
+                    <div className="cash">
+                      <p>-N200</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <div className="recent-trans">
+                    <div>
+                      <p className="date">3-28-2022</p>
+                      <img src={imgUrl10} />
+                    </div>
+                    <div className="water-time">
+                      <p className="water">Pocket Moni</p>
+                      <p className="time">10:00 am</p>
+                    </div>
+                    <div className="cash">
+                      <p className="moni">N5,000</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <div className="recent-trans">
+                    <div>
+                      <p className="date">3-28-2022</p>
+                      <img src={imgUrl11} />
+                    </div>
+                    <div className="water-time">
+                      <p className="water">Up-keep</p>
+                      <p className="time">10:00 am</p>
+                    </div>
+                    <div className="cash">
+                      <p className="up-keep">N5,000</p>
+                    </div>
+                  </div>
+                </div>
+              
             </div>
-
-
-
-           {/* <div className="backgroung-img-div">
-             <img className="background-img" src={imgUrl2} alt="" />
-           </div>
-      
-            <div className="greeting-card">
-              <div className="greeting-div">
-                <p className="greeting-paragraph">Hi Kenny,</p>
-                <div className="userimg-div">
-                  <img className="user-img" src={imgUrl12} alt="Trulli" />
-                </div>
-
-                <div className="amountcard-div">
-                  <div className="amount-card">
-                    <div className="amount-div">
-                      <p className="amount-paragraph">Total Balance</p>
-                      <p>£1m</p>
-                      <img className="bell-img" src={imgUrl13} alt="Trulli" />
-                      <img className="eye-img" src={imgUrl14} alt="Trulli" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="images-div">
-                <img className="atm-img" src={imgUrl3} alt="Trulli" />
-                <p className="atm-card">cards</p>
-                <img className="spend-img" src={imgUrl4} alt="Trulli" />
-                <p>Spend</p>
-                <img className="help-img" src={imgUrl5} alt="Trulli" />
-                <p>Help</p>
-                <img className="airtime-img" src={imgUrl6} alt="Trulli" />
-                <p>Airtime</p>
-                <img className="save-img" src={imgUrl7} alt="Trulli" />
-                <p>Save</p>
-              </div>
-
-              <div className="transaction-div">
-                <p className="transaction-p">Recent Transactions</p>
-              </div>
-
-              <div className="waterpayment-div">
-                <div className="waterpayment-card">
-                  <div className="waterpaymentsecond-div">
-                    <p>Water Payment</p>
-                    <p>10:00am</p>
-                    <p>-$1,000</p>
-
-                    <img className="ellipse-img" src={imgUrl8} alt="Trulli" />
-                  </div>
-                </div>
-
-                <div className="airtimecard-div">
-                  <div className="airtime-card">
-                    <div className="airtime-div">
-                      <img className="ellipse-img" src={imgUrl9} alt="Trulli" />
-                      <p className="airtime-p">Airtime</p>
-                      <p>10:00am</p>
-                      <p>-$200</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="pocketmoni-div">
-                  <div className="pocketmoni-card">
-                    <div className="pocket-div">
-                      <p>Pocket Moni</p>
-                      <p>10:00am</p>
-                      <p>$5,000</p>
-
-                      <img className="ellipse-img" src={imgUrl10} alt="Trulli" />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="upkeep-div">
-                  <div className="upkeep-card">
-                    <p>Up-keep</p>
-                    <p>10:00am</p>
-                    <p>$5,000</p>
-
-                    <img className="ellipse-img" src={imgUrl11} alt="Trulli" />
-                  </div>
-                </div>
-              </div>
-            </div> */}
             </div>
             
          
